392e47be2617cb469920e111ea48fdd0c9c1c2c6
jgamper <jevgenij.gamper5@gmail.com>
2020-04-24 23:11:50 +0100

    refactored to be more terminal based using docopt

--- src/config/config.py
+++ src/config/config.py
@@ -7,19 +7,19 @@ device_id = 1
 batch_size = 32
 
 tasks = {
-        'BREAST': {'stats': {'mean': (0.6685, 0.4844, 0.6638), 'std': (0.2205, 0.2397, 0.1844)},
+        'breast': {'stats': {'mean': (0.6685, 0.4844, 0.6638), 'std': (0.2205, 0.2397, 0.1844)},
                      'num_classes': 2},
-        'COLON': {'stats': {'mean': (0.7348, 0.5770, 0.7010), 'std': (0.2241, 0.2842, 0.2276)},
+        'colon': {'stats': {'mean': (0.7348, 0.5770, 0.7010), 'std': (0.2241, 0.2842, 0.2276)},
                      'num_classes': 9},
-        'LYMPHOMA': {'stats': {'mean': (0.4125, 0.3381, 0.4149), 'std': (0.2068, 0.1937, 0.1913)},
+        'lymphoma': {'stats': {'mean': (0.4125, 0.3381, 0.4149), 'std': (0.2068, 0.1937, 0.1913)},
                      'num_classes': 3},
-        'LUNG': {'stats': {'mean': (0.7680, 0.6133, 0.7297), 'std': (0.1867, 0.2556, 0.2011)},
+        'lung': {'stats': {'mean': (0.7680, 0.6133, 0.7297), 'std': (0.1867, 0.2556, 0.2011)},
                       'num_classes': 6},
-        'OVARY': {'root': '/media/mount2/AllTheTasks/Pathology/PatchClassification/ovarian/split',
+        'ovary': {'root': '/media/mount2/AllTheTasks/Pathology/PatchClassification/ovarian/split',
                   'stats': {'mean': (0.7301, 0.5199, 0.6619), 'std': (0.1690, 0.2129, 0.1583)},
                   'num_classes': 5},
-        'ORAL': {'stats': {'mean': (0.7960, 0.7192, 0.8201), 'std': (0.1547, 0.1710, 0.1152)},
+        'oral': {'stats': {'mean': (0.7960, 0.7192, 0.8201), 'std': (0.1547, 0.1710, 0.1152)},
                    'num_classes': 3},
-        'MENINGIOMA': {'stats': {'mean': (0.5648, 0.3882, 0.5284), 'std': (0.2492, 0.2009, 0.2280)},
+        'meningioma': {'stats': {'mean': (0.5648, 0.3882, 0.5284), 'std': (0.2492, 0.2009, 0.2280)},
                   'num_classes': 4}
          }
